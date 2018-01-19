1d272a1927146e72a921d6edf8241348ee55b3c5
mameloga@yahoo.fr <mameloga@hotmail.fr>
2018-01-19 01:59:02 -0500

    add email intent

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,5 +1,5 @@
 /**
- * IMPORTANT: Make sure you are using the correct package name. 
+ * IMPORTANT: Make sure you are using the correct package name.
  * This example uses the package name:
  * package com.example.android.justjava
  * If you get an error when copying this code into Android studio, update it to match teh package name found
@@ -9,20 +9,24 @@
 package com.example.android.justjava;
 
 
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.NumberFormat;
+
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 1;
 
 
     @Override
@@ -44,10 +48,19 @@ public class MainActivity extends AppCompatActivity {
         boolean haschocolate = chocolateCheckBox.isChecked();
         int price = calculatePrice(hasWhippedCream, haschocolate);
         String priceMessage = createOderSummary(name, price, hasWhippedCream, haschocolate);
-        displayMessage(priceMessage);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java Order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+
     }
 
-    private String createOderSummary(String name, int price, boolean addwhippedCream, boolean addchocolate){
+    private String createOderSummary(String name, int price, boolean addwhippedCream, boolean addchocolate) {
 
         String priceMessage = "Name: " + name;
         priceMessage += "\nAdd whipped cream?" + addwhippedCream;
@@ -61,11 +74,11 @@ public class MainActivity extends AppCompatActivity {
     private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
         int basePrice = 5;
 
-        if(addWhippedCream) {
+        if (addWhippedCream) {
             basePrice += 1;
         }
 
-        if(addChocolate) {
+        if (addChocolate) {
             basePrice += 2;
         }
 
@@ -73,19 +86,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increment(View view) {
+        if (quantity == 100) {
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity + 1;
         displayQuantity(quantity);
 
     }
 
 
-
     public void decrement(View view) {
-        quantity = quantity -1;
+        if (quantity == 1) {
+            Toast.makeText(this, "You cannot have less than 1 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity = quantity - 1;
         displayQuantity(quantity);
 
     }
-
 
 
     /**
@@ -94,11 +113,6 @@ public class MainActivity extends AppCompatActivity {
     private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-    }
-
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
     }
 
 }
